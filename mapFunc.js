1c110a5f0595cd8c39f5f41dd2c1b3e228df985e
TAKE1731 <tak-imanishi@ti.jp.nec.com>
2016-12-17 16:27:11 +0900

    いまにし追加
    CSV読み込み処理追加

--- mapFunc.js
+++ mapFunc.js
@@ -1,4 +1,4 @@
-
+﻿
 //var ikedaLatLng = new google.maps.LatLng(35.890358,136.344221);
 
 //var onomichiLatLng = new google.maps.LatLng(34.404839,133.193653);
@@ -113,4 +113,28 @@ function setGeoMarker(r, lat, lng){
          fillOpacity: 0.2
     });
 */
+}
+
+function getCSV(){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", ".\data\art.csv", false); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+	
+    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
+}
+
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var result = []; // 最終的な二次元配列を入れるための配列
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+ 
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+ 
+    alert(result[1][2][3][4]); 
 }
